4eca72db4c3a0a77e0b6107f5f5acf46eea693ed
GitCreature <pasha29091998@gmail.com>
2018-12-22 13:51:31 +0500

    simple logic

---
+++ src/components/Card/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './style.css';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+    constructor(props){
+        super(props);
+        this.opened = props.opened;
+        this.onClick = props.onClick;
+    }
+    render () {
+        return (
+            <div className={this.opened ? styles.cardFront : styles.cardBack} onClick={this.onClick}>
+
+
+            </div>
+        );
+    }
+}
+
+Card.propTypes = {
+    opened: PropTypes.bool,
+    onClick: PropTypes.func,
+};
+
+export default Card;
--- src/components/Memory/Memory.js
+++ src/components/Memory/Memory.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import styles from './style.css';
+import Card from "../Card/Card";
+import PropTypes from 'prop-types'
+
+const modes = {
+    easy: {
+        width: 5,
+        height: 4,
+    }
+};
 
 export default class Memory extends React.Component {
-    render () {
-        return (
+    constructor(props) {
+        super(props);
+        if(!modes[props.mode]) {
+            this.mode = 'easy';
+        }
+        else {
+            this.mode = props.mode;
+        }
+    }
 
+    render() {
+        return (
+            <div className={styles.wrapper}>
+                {renderCards()}
+            </div>
         );
     }
+
+    handleClick = event => {
+        if(event.target.opened){
+            event.target.opened=!event.target.opened;
+        }
+    }
+
+}
+
+Memory.propTypes = {
+    mode: PropTypes.string,
+};
+
+function renderCards(onClick) {
+    let cards = [];
+    for(let i = 0; i < modes[this.mode].width*modes[this.mode].height; i++){
+        cards.push(<Card opened={false} onClick={onClick}/>)
+    }
+    return cards;
 }
