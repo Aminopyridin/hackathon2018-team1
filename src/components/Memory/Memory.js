83083acd75e0d77bf65c05fe929c9a7f69602a94
miurri <anna.25.03_98@mail.ru>
2018-12-22 14:41:58 +0500

    memory add

--- src/components/Memory/Memory.js
+++ src/components/Memory/Memory.js
@@ -2,12 +2,30 @@ import React from 'react';
 import styles from './style.css';
 
 export default class Memory extends React.Component {
-    render () {
+    render() {
         return (
-            <div className={styles.memory}>
-                <h1>Мем.Ори!</h1>
-
-                <div className={styles.cell} />
+            <div className={styles.game}>
+                <h1 className={styles.title}>Мем.Ори!</h1>
+                <div className={styles.field}>
+                    <div className={styles.memory}>
+                        <div className={`${styles.cell} ${styles.ball1}`}/>
+                        <div className={`${styles.cell} ${styles.ball2}`}/>
+                        <div className={`${styles.cell} ${styles.ball3}`}/>
+                        <div className={`${styles.cell} ${styles.ball4}`}/>
+                        <div className={`${styles.cell} ${styles.ball5}`}/>
+                        <div className={`${styles.cell} ${styles.ball6}`}/>
+                        <div className={`${styles.cell} ${styles.ball7}`}/>
+                        <div className={`${styles.cell} ${styles.ball8}`}/>
+                        <div className={styles.cell}/>
+                        <div className={styles.cell}/>
+                        <div className={styles.cell}/>
+                        <div className={styles.cell}/>
+                        <div className={styles.cell}/>
+                        <div className={styles.cell}/>
+                        <div className={styles.cell}/>
+                        <div className={styles.cell}/>
+                    </div>
+                </div>
             </div>
         );
     }
